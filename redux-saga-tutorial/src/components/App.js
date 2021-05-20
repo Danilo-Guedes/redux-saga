3bdfe652379ef96bdd05855ede87c93a69a3b12b
Danilo Guedes <danilotiba@gmail.com>
2021-05-20 16:33:57 -0300

    dispatched getUsers action to console.log the list of users from api call

--- redux-saga-tutorial/src/components/App.js
+++ redux-saga-tutorial/src/components/App.js
@@ -1,19 +1,32 @@
-// LEARNING ABOUT GENERATORS
-// function* testing() {
-//     while (true) {
-//         yield 1
-//         yield 2
-//         yield 3
-//     }
-// }
+import { useEffect } from 'react'
+import { getUsersRequest } from '../actions/users'
+import { useDispatch } from 'react-redux'
+
+/*
+LEARNING ABOUT GENERATORS
+function* testing() {
+    while (true) {
+        yield 1
+        yield 2
+        yield 3
+    }
+}
+*/
 
 function App() {
+    /*
     // const iterator = testing()
     // console.log(iterator.next())
     // console.log(iterator.next())
     // console.log(iterator.next())
     // console.log(iterator.next())
+*/
 
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getUsersRequest())
+    }, [dispatch])
 
     return <div>DANILO</div>
 }
