b5bd7061de6a86db7abc57ee48eab9a8ea1292b8
Danilo Guedes <danilotiba@gmail.com>
2021-05-20 16:08:58 -0300

    used createSagaMiddleware to connect Sagas with Redux

--- redux-saga-tutorial/src/index.js
+++ redux-saga-tutorial/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import reportWebVitals from './reportWebVitals'
 import axios from 'axios'
 
 import reducers from './reducers'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './sagas'
 
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api'
 
-const store = createStore(reducers)
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
     <Provider store={store}>
@@ -22,7 +27,4 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+
