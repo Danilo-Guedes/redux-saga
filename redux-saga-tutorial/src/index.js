a07a0d298af48d144f0f129f0f48d7b471ba8c61
Danilo Guedes <danilotiba@gmail.com>
2021-05-20 15:18:50 -0300

    created basic actions reducers and store to finally use sagas

--- redux-saga-tutorial/src/index.js
+++ redux-saga-tutorial/src/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import reportWebVitals from './reportWebVitals'
 import axios from 'axios'
+
+import reducers from './reducers'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api'
 
+const store = createStore(reducers)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
